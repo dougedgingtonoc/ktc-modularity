85784b18035ecbffa7eda3f36208f54273f64f3f
Douglas Edgington <doug@oncecoupled.com>
2017-10-01 23:28:33 -0400

    Laying out the theme’s architecture part 2

--- developers/lib/functions/formatting.php
+++ developers/lib/functions/formatting.php
@@ -2,10 +2,10 @@
 /**
  * Description
  *
- * @package     OnceCoupled\Developers
+ * @package     OnceCoupled\Developers\
  * @since       1.0.0
  * @author      Doug OC
  * @link        https://www.oncecoupled.com/
  * @license     GNU General Public License 2.0+
  */
-namespace OnceCoupled\Developers;
+namespace OnceCoupled\Developers\;
